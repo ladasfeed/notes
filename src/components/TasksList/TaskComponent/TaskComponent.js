f4551a240df489be0d09959aa0433c58eba14b47
grygoryz <grigoryts@yahoo.com>
2020-07-09 17:51:39 +0300

    task component

--- src/components/TasksList/TaskComponent/TaskComponent.js
+++ src/components/TasksList/TaskComponent/TaskComponent.js
@@ -1,12 +1,20 @@
 import React from "react";
 import c from "./TaskComponent.module.css";
 
-const TaskComponent = ({title}) => {
+const TaskComponent = ({title, description}) => {
 
     return (
         <div className={c.container}>
             <div className={c.title}>Title text</div>
-            <div className={c.description}>Description</div>
+            <div className={c.description}>
+                <p>This is the description. Lorem ipsum huipsum. Yo yo yo
+                    This is the description. </p>
+            </div>
+            <div className={c.footer}>
+                <label><input type="checkbox"/> Done</label>
+                <button>Edit</button>
+                <button>Remove</button>
+            </div>
         </div>
     )
 };
